1db1ff938134237207b13d20c4414fa43ab33745
natrix1 <141676108+natrix1@users.noreply.github.com>
2023-10-02 17:33:15 -0300

    clase producto

--- src/proyectoventasg23/Entidades/Producto.java
+++ src/proyectoventasg23/Entidades/Producto.java
@@ -9,5 +9,72 @@ public class Producto {
     double precioActual;
     int stock;
     boolean estado;
+
+    public Producto() {
+    }
+
+    public Producto(int idProducto, String nombreProducto, String Descripcion, double precioActual, int stock, boolean estado) {
+        this.idProducto = idProducto;
+        this.nombreProducto = nombreProducto;
+        this.Descripcion = Descripcion;
+        this.precioActual = precioActual;
+        this.stock = stock;
+        this.estado = estado;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    public double getPrecioActual() {
+        return precioActual;
+    }
+
+    public void setPrecioActual(double precioActual) {
+        this.precioActual = precioActual;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "idProducto=" + idProducto + ", nombreProducto=" + nombreProducto + ", Descripcion=" + Descripcion + ", precioActual=" + precioActual + ", stock=" + stock + ", estado=" + estado + '}';
+    }
+    
+    
     
 }
