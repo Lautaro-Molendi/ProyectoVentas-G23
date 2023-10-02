68bb649e9ae190b6d378982b9c481c4e67807f00
lucianaamet <luciana_amet@hotmail.com>
2023-10-02 17:07:23 -0300

    carpetaLib
    cree la carpeta de las librerias

--- src/proyectoventasg23/Entidades/Cliente.java
+++ src/proyectoventasg23/Entidades/Cliente.java
@@ -6,6 +6,26 @@ public class Cliente {
     String nombre; 
     String apellido;
     String domicilio;
-    String telfono;
+    String telefono;
+
+    public Cliente() {
+    }
+
+    public Cliente(int idCliente, String nombre, String apellido, String domicilio, String telefono) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+    }
+
+    public Cliente(String nombre, String apellido, String domicilio, String telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+    }
+    
+    
     
 }
