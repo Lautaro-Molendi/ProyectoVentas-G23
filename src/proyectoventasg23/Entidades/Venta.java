461866812135963abf50c51ae17196c0252ec02e
natrix1 <141676108+natrix1@users.noreply.github.com>
2023-10-02 17:37:55 -0300

    clase venta

--- src/proyectoventasg23/Entidades/Venta.java
+++ src/proyectoventasg23/Entidades/Venta.java
@@ -8,5 +8,44 @@ public class Venta {
    int idVenta;
    Cliente cliente; 
    LocalDate FechaVenta;
+
+    public Venta() {
+    }
+
+    public Venta(int idVenta, Cliente cliente, LocalDate FechaVenta) {
+        this.idVenta = idVenta;
+        this.cliente = cliente;
+        this.FechaVenta = FechaVenta;
+    }
+
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDate getFechaVenta() {
+        return FechaVenta;
+    }
+
+    public void setFechaVenta(LocalDate FechaVenta) {
+        this.FechaVenta = FechaVenta;
+    }
+
+    @Override
+    public String toString() {
+        return "Venta{" + "idVenta=" + idVenta + ", cliente=" + cliente + ", FechaVenta=" + FechaVenta + '}';
+    }
+   
    
 }
