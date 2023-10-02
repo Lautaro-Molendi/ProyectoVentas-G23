850b03b80a4f75342f643ee23c02774e9ccce795
Lautaro-Molendi <lsmolendi@gmail.com>
2023-10-02 16:00:01 -0300

    Initial commit

---
+++ src/proyectoventasg23/AccesoADatos/Conexion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoventasg23.AccesoADatos;
+
+/**
+ *
+ * @author suyay
+ */
+public class Conexion {
+    
+}
---
+++ src/proyectoventasg23/Entidades/Venta.java
@@ -0,0 +1,12 @@
+
+package proyectoventasg23.Entidades;
+
+import java.time.LocalDate;
+
+public class Venta {
+    
+   int idVenta;
+   Cliente cliente; 
+   LocalDate FechaVenta;
+   
+}
