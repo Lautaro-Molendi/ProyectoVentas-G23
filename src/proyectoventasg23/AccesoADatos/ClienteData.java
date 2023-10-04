4b2f67af1b3cc165a3b1fc1946711db253dc6978
lucianaamet <luciana_amet@hotmail.com>
2023-10-04 18:13:01 -0300

    clienteData
    alta, baja real, modificacion y lista...buscar por id me tira error de sintaxis. Debe ser una pavada pero estoy quemada... mañana veremos!!!

--- src/proyectoventasg23/AccesoADatos/ClienteData.java
+++ src/proyectoventasg23/AccesoADatos/ClienteData.java
@@ -1,10 +1,126 @@
 
 package proyectoventasg23.AccesoADatos;
 
-/**
- *
- * @author luciana
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import proyectoventasg23.Entidades.*;
+
+
+
 public class ClienteData {
-    //st
+      private Connection connection = null;
+    
+    public ClienteData(){
+        connection = Conexion.getConexion();         
+  }
+    public void guardarCliente (Cliente cliente){
+        String sql = "INSERT INTO cliente (apellido, nombre, domicilio, telefono)"
+                + " VALUES (?, ?, ?, ?)";
+        
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1,cliente.getApellido());
+            ps.setString(2, cliente.getNombre());
+            ps.setString(3, cliente.getDomicilio());
+            ps.setString(4, cliente.getTelefono());
+            ps.executeUpdate();
+            
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()){
+                cliente.setIdCliente(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Cliente registrado");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "No se pudo acceder a la tabla de clientes");
+        }
+      }     
+     public void modificarCliente(Cliente cliente){
+        String sql = "UPDATE cliente SET apellido= ?, nombre= ?, domicilio= ?, telefono= ?"
+                + " WHERE idCliente= ? ";
+        
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, cliente.getApellido());
+            ps.setString(2, cliente.getNombre());
+            ps.setString (3, cliente.getDomicilio());
+            ps.setString(4, cliente.getTelefono());
+            ps.setInt(5, cliente.getIdCliente());
+            int exito = ps.executeUpdate();
+            
+            if (exito==1){
+                JOptionPane.showMessageDialog(null, "Cliente modificado");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "No se pudo acceder a la tabla de clientes");
+        }
+  }
+      public void eliminarCliente (int id){
+        String sql = "DELETE FROM cliente WHERE idCliente = ?";
+        
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, id);
+           int exito = ps.executeUpdate();
+           if (exito==1){
+               JOptionPane.showMessageDialog(null, "Cliente eliminado");
+           }
+            } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "No se pudo acceder a la tabla de clientes ");
+        }
+}
+       public List <Cliente> listarCliente(){
+    List <Cliente> clientes = new ArrayList <Cliente>();
+    String sql = "SELECT idCliente, apellido, nombre, domicilio, telefono"
+            + " FROM cliente";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+            Cliente cliente = new Cliente();
+            cliente.setIdCliente(rs.getInt("idCliente"));
+            cliente.setApellido(rs.getString("apellido"));
+            cliente.setNombre(rs.getString("nombre"));
+            cliente.setDomicilio(rs.getString("domicilio"));
+            cliente.setTelefono(rs.getString("telefono"));
+            clientes.add(cliente);             
+        }
+            System.out.print(" ");
+         ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "No se pudo acceder a la tabla de clientes");
+        }
+    return clientes;
+          
+  } 
+      public Cliente buscarCliente(int id) {
+        Cliente cliente = null;
+        String sql = "SELECT apellido, nombre, domicilio, telefono,"
+                + " FROM cliente WHERE idCliente = ? ";
+        PreparedStatement ps = null;
+        try {
+            ps = connection.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                cliente = new Cliente();
+                
+                cliente.setIdCliente(rs.getInt(id));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setDomicilio(rs.getString("domicilio"));
+                cliente.setTelefono(rs.getString("telefono"));
+            }else{JOptionPane.showMessageDialog(null, "No existe el Cliente buscado");}
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "No se pudo acceder a la tabla de clientes");
+        }
+        return cliente;
+    }
 }
--- src/proyectoventasg23/ProyectoVentasG23.java
+++ src/proyectoventasg23/ProyectoVentasG23.java
@@ -3,8 +3,8 @@ package proyectoventasg23;
 
 import java.sql.Connection;
 import proyectoventasg23.AccesoADatos.Conexion;
-import proyectoventasg23.AccesoADatos.ProductoData;
-import proyectoventasg23.Entidades.Producto;
+import proyectoventasg23.AccesoADatos.*;
+import proyectoventasg23.Entidades.*;
 
 public class ProyectoVentasG23 {
 
@@ -12,13 +12,22 @@ public class ProyectoVentasG23 {
     public static void main(String[] args) {
     Connection connection = Conexion.getConexion();
     // Producto p1 = new Producto (6, "azucar", "alimento seco", 750, 16, true);
-      ProductoData pD = new ProductoData ();
+      //ProductoData pD = new ProductoData ();
        //pD.guardarProducto(p1);
         //pD.eliminarProducto(11);
         //pD.modificarProducto(p1);
-       // System.out.println(pD.listarProducto());
-        System.out.println(pD.buscarProducto(6)); 
+        //System.out.println(pD.listarProducto());
+        //System.out.println(pD.buscarProducto(2)); 
        
+        //Cliente c1= new Cliente("Gomez", "Carlos", "Rosas 888", "228154268");
+        //Cliente c2= new Cliente("Perez", "Mario", "Mitre 333", "2265982268");
+        //Cliente c3= new Cliente(3,"Martinez", "Luis", "Maipu 2020", "365154268");
+        ClienteData cd= new ClienteData();
+        //cd.guardarCliente(c3);
+        //cd.modificarCliente(c1);
+       // cd.eliminarCliente(1);
+        //System.out.print(cd.listarCliente());
+          cd.buscarCliente(3);
     }
     
 }
