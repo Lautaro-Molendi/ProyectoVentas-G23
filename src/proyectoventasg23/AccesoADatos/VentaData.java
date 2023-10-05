708e9defc3f5160beab7e563546ca905160f70ac
Lautaro-Molendi <lsmolendi@gmail.com>
2023-10-05 17:49:47 -0300

    Sumo ventaData (tiene un error que mañana corrijo))
    Github por algún motivo me borró todos los cambios cuando quise mandar la versión anterior, y ahora tengo que irme. A la noche subo la clase de detalle. Mañana lo corrijo.

--- src/proyectoventasg23/AccesoADatos/VentaData.java
+++ src/proyectoventasg23/AccesoADatos/VentaData.java
@@ -1,14 +1,126 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyectoventasg23.AccesoADatos;
 
-/**
- *
- * @author luciana
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDate;
+import javax.swing.JOptionPane;
+import proyectoventasg23.Entidades.*;
+
 public class VentaData {
-    //ss
+    
+    private Connection connection = null;
+
+    public VentaData() {
+        connection = Conexion.getConexion();
+    }
+
+    public void guardarVenta(Venta venta) {
+        String sql = "INSERT INTO venta (idCliente, fechaVenta) VALUES (?, ?)";
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, venta.getCliente().getIdCliente());
+            ps.setDate(2, java.sql.Date.valueOf(venta.getFechaVenta()));
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                venta.setIdVenta(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Venta registrada");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "No se pudo acceder a la tabla de ventas");
+        }
+    }
+
+    public void modificarVenta(Venta venta) {
+        String sql = "UPDATE venta SET idCliente = ?, fechaVenta = ? WHERE idVenta = ?";
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, venta.getCliente().getIdCliente());
+            ps.setDate(2, java.sql.Date.valueOf(venta.getFechaVenta()));
+            ps.setInt(3, venta.getIdVenta());
+            int exito = ps.executeUpdate();
+
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Venta modificada");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "No se pudo acceder a la tabla de ventas");
+        }
+    }
+
+    public void eliminarVenta(int id) {
+        String sql = "DELETE FROM venta WHERE idVenta = ?";
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, id);
+            int exito = ps.executeUpdate();
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Venta eliminada");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "No se pudo acceder a la tabla de ventas");
+        }
+    }
+    
+   public List<Venta> listarVentas() {
+    List<Venta> ventas = new ArrayList<>();
+    String sql = "SELECT idVenta, idCliente, fechaVenta FROM venta";
+
+    try {
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            Venta venta = new Venta();
+            venta.setIdVenta(rs.getInt("idVenta"));
+            int idCliente = rs.getInt("idCliente");
+            ClienteData clienteData = new ClienteData();
+            Cliente cliente = clienteData.buscarCliente(idCliente);
+            
+            venta.setCliente(cliente);
+            venta.setFechaVenta(rs.getDate("fechaVenta").toLocalDate());
+            ventas.add(venta);
+        }
+        ps.close();
+    } catch (SQLException ex) {
+        JOptionPane.showMessageDialog(null, "No se pudo acceder a la tabla de ventas");
+    }
+    return ventas;
+}
+
+public Venta buscarVenta(int id) {
+    Venta venta = null;
+    String sql = "SELECT idCliente, fechaVenta FROM venta WHERE idVenta = ?";
+    PreparedStatement ps = null;
+    try {
+        ps = connection.prepareStatement(sql);
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()) {
+            venta = new Venta();
+            venta.setIdVenta(id);
+            int idCliente = rs.getInt("idCliente");
+            ClienteData clienteData = new ClienteData();
+            Cliente cliente = clienteData.buscarCliente(idCliente); //tiene un problema para acceder al cleinte.
+            
+            venta.setCliente(cliente);
+            venta.setFechaVenta(rs.getDate("fechaVenta").toLocalDate());
+        } else {
+            JOptionPane.showMessageDialog(null, "No existe la Venta buscada");
+        }
+        ps.close();
+    } catch (SQLException ex) {
+        JOptionPane.showMessageDialog(null, "No se pudo acceder a la tabla de ventas");
+    }
+    return venta;
+}
 }
