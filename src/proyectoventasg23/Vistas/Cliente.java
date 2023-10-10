146832ef3dad1b07b6db46cd884f0b2e7b35f401
lucianaamet <luciana_amet@hotmail.com>
2023-10-10 15:54:32 -0300

    a la clase creada
    movemos y eliminamos lo que sobra

--- src/proyectoventasg23/Vistas/Cliente.java
+++ src/proyectoventasg23/Vistas/Cliente.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package vistafravemax.vista;
+package proyectoventasg23.Vistas;
 
 /**
  *
--- src/proyectoventasg23/Vistas/menuPpal.java
+++ src/proyectoventasg23/Vistas/menuPpal.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package vistafravemax.vista;
+package proyectoventasg23.Vistas;
 
 
 public class menuPpal extends javax.swing.JFrame {
