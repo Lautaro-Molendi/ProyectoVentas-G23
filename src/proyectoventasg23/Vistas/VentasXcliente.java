f7ad1d14d37ad9cbedd710b6f5f659e2ccbd6bc4
Lautaro-Molendi <lsmolendi@gmail.com>
2023-10-26 15:41:38 -0300

    VentasXcliente

--- src/proyectoventasg23/Vistas/VentasXcliente.java
+++ src/proyectoventasg23/Vistas/VentasXcliente.java
@@ -5,6 +5,11 @@
  */
 package proyectoventasg23.Vistas;
 
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+import proyectoventasg23.AccesoADatos.VentaData;
+import proyectoventasg23.Entidades.Venta;
+
 /**
  *
  * @author luciana
@@ -32,7 +37,7 @@ public class VentasXcliente extends javax.swing.JInternalFrame {
         jTextCliente = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        jTVentas = new javax.swing.JTable();
 
         setClosable(true);
         setTitle("Busqueda de ventas por cliente");
@@ -40,12 +45,16 @@ public class VentasXcliente extends javax.swing.JInternalFrame {
         jPanel1.setBackground(new java.awt.Color(255, 102, 102));
 
         jLabel1.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
         jLabel1.setText("Ingrese el Cliente");
 
         jButton1.setText("Buscar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jTVentas.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -56,7 +65,7 @@ public class VentasXcliente extends javax.swing.JInternalFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(jTVentas);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -105,13 +114,35 @@ public class VentasXcliente extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+    int clienteID = Integer.parseInt(jTextCliente.getText());
+
+    DefaultTableModel model = new DefaultTableModel();
+    model.addColumn("ID Venta");
+    model.addColumn("Fecha Venta");
+
+    model.setRowCount(0);
+
+    VentaData ventaData = new VentaData();
+    List<Venta> todasLasVentas = ventaData.listarVentas();
+
+    for (Venta venta : todasLasVentas) {
+        if (venta.getCliente().getIdCliente() == clienteID) {
+            model.addRow(new Object[]{venta.getIdVenta(), venta.getFechaVenta()});
+        }
+    }
+
+    jTVentas.setModel(model);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTVentas;
     private javax.swing.JTextField jTextCliente;
     // End of variables declaration//GEN-END:variables
 }
