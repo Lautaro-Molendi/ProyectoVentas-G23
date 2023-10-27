2be7d3d921e091c26f386132736a91975c4becf7
Lautaro-Molendi <lsmolendi@gmail.com>
2023-10-27 16:30:15 -0300

    Tabla VentasXfecha

--- src/proyectoventasg23/Vistas/VentasXfecha.java
+++ src/proyectoventasg23/Vistas/VentasXfecha.java
@@ -38,13 +38,13 @@ public class VentasXfecha extends javax.swing.JInternalFrame {
 
         jTVentas.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "ID Venta", "Cleinte", "Fecha Venta"
             }
         ));
         jScrollPane1.setViewportView(jTVentas);
